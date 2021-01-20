b1e6e0e74b88027ecec854923e7df5a75e479279
bfank9@gmail.com <bfank9@gmail.com>
2021-01-20 16:22:43 -0700

    Add project files.

---
+++ wwwroot/js/GradeCalculator.js
@@ -0,0 +1,61 @@
+﻿var submit = document.getElementById("submit");
+
+
+submit.addEventListener("click", () => {
+    this.CalculateGrade();
+});
+
+function CalculateGrade() {
+
+    var iAssignmentScore = parseInt(document.getElementById("assignmentsPercent").value);
+    var iGroupProjectScore = parseInt(document.getElementById("GroupProjectPercent").value);
+    var iQuizzesScore = parseInt(document.getElementById("QuizzesPercent").value);
+    var iExamsScore = parseInt(document.getElementById("ExamsPercent").value);
+    var iIntexScore = parseInt(document.getElementById("IntexPercent").value);
+    var iFinalGrade = 0;
+    var iLetterGrade = '';
+
+    iFinalGrade = iAssignmentScore + iGroupProjectScore + iQuizzesScore + iExamsScore + iIntexScore;
+
+    if (iFinalGrade >= 94) {
+        iLetterGrade = "A"
+    }
+    else if (iFinalGrade >= 90) {
+        iLetterGrade = "A-"
+    }
+    else if (iFinalGrade >= 87) {
+        iLetterGrade = "B+"
+    }
+    else if (iFinalGrade >= 84) {
+        iLetterGrade = "B"
+    }
+    else if (iFinalGrade >= 80) {
+        iLetterGrade = "B-"
+    }
+    else if (iFinalGrade >= 77) {
+        iLetterGrade = "C+"
+    }
+    else if (iFinalGrade >= 74) {
+        iLetterGrade = "C"
+    }
+    else if (iFinalGrade >= 70) {
+        iLetterGrade = "C-"
+    }
+    else if (iFinalGrade >= 67) {
+        iLetterGrade = "D+"
+    }
+    else if (iFinalGrade >= 64) {
+        iLetterGrade = "D"
+    }
+    else if (iFinalGrade >= 60) {
+        iLetterGrade = "D-"
+    }
+    else {
+        iLetterGrade = "E"
+    }
+
+    var output = "Final Percent Grade:<br><br> " + iFinalGrade + "%<br><br>" + "Final Letter Grade:<br><br> " + iLetterGrade; 
+
+    document.getElementById("output").innerHTML = output;
+    
+}
